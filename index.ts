5f9a387a183c253f8ef5bc0861e6d14f304f7a88
alans <alan@compie.co.il>
2023-08-16 17:05:35 +0300

    Added Dockerfile

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, {Express, Request, Response}  from "express";
-const port = 8000;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.get("/", (req, res)=> {
-    res.send("HELLO FROM EXPRESS + TS!")
+    res.json({
+        message:"Docker is easy!"
+    })
 })
 
 app.listen(port, ()=> {
